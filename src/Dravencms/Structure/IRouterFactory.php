57d8d5eeb0cd6b9d9486181be70fcbdffdce8084
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-27 00:29:46 +0100

    Fix

---
+++ src/Dravencms/Structure/IRouterFactory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+
+namespace Salamek\Structure;
+
+
+interface IRouterFactory
+{
+
+}
---
+++ src/Dravencms/Structure/RouterFactory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+
+namespace Salamek\Structure;
+
+use Nette\Application\Routers\RouteList;
+
+/**
+ * Class RouteFactory
+ * @package Salamek\Cms
+ */
+class RouterFactory
+{
+    /** @var array */
+    private $routeFactories = [];
+
+    /**
+     * @param IRouterFactory $routeFactory
+     */
+    public function addRouteFactory(IRouterFactory $routeFactory)
+    {
+        $this->routeFactories[] = $routeFactory;
+    }
+
+    /**
+     * @return array|RouteList
+     */
+    public function createRouter()
+    {
+        $router = new RouteList();
+        foreach ($this->routeFactories AS $routeFactory) {
+            $router[] = $routeFactory->createRouter();
+        }
+
+        return $router;
+    }
+}
